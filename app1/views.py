35c5b7a7fb27d625b8febec308e5d22c856c232c
shwetha3341 <shwetha3341@gmail.com>
2021-11-29 00:18:36 -0800

    first commit

---
+++ app1/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def boot1(request):
+    return render(request,'boot1.html')
+
+def boot2(request):
+    return render(request,'boot2.html')    
